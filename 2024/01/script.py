a19a58dd8080c38f63df382318c47fbd810d6870
shaytan1986 <tetroxide@gmail.com>
2024-12-01 15:19:00 -0700

    01, 2024

---
+++ 2024/01/script.py
@@ -0,0 +1,15 @@
+def solve():
+    items = [
+        (a[0], a[1])
+        for a in
+        [item.split("\t") for item in open("input")]
+    ]
+    for a, b in items:
+        print(f"a: {a}")
+        print(f"b: {b}")
+        returncd 01
+    
+
+    
+if __name__ == '__main__':
+    solve()
